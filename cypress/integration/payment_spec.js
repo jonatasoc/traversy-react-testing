43677f8e16677d9ff80e0cea86b9dda10bdb1a2d
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2021-11-19 23:32:46 -0300

    chore: add cypress

---
+++ cypress/integration/payment_spec.js
@@ -0,0 +1,16 @@
+const { verify } = require("crypto");
+
+describe("payment", () => {
+  it("user can make payment", () => {
+    // login
+    // check account balance
+    // click on pay button
+    // search for user
+    // add amount and note and click pay
+    // return to the transactions
+    // go to personal payments
+    // click on payment
+    // verify if payment wad made
+    //verify if payment amount was deducted
+  });
+});
