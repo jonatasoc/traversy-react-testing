7659ab9096823daffc60593174d189b5d912a371
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2021-11-20 00:13:12 -0300

    feat: add first e2e test

--- cypress/integration/payment_spec.js
+++ cypress/integration/payment_spec.js
@@ -1,8 +1,11 @@
-const { verify } = require("crypto");
-
 describe("payment", () => {
   it("user can make payment", () => {
     // login
+    cy.visit("/");
+    cy.findByRole("textbox", { name: /username/i }).type("johndoe");
+    cy.findByLabelText(/password/i).type("s3cret");
+    cy.findByRole("checkbox", { name: /remember me/i }).check();
+    cy.findByRole("button", { name: /sign in/i }).click();
     // check account balance
     // click on pay button
     // search for user
@@ -11,6 +14,6 @@ describe("payment", () => {
     // go to personal payments
     // click on payment
     // verify if payment wad made
-    //verify if payment amount was deducted
+    // verify if payment amount was deducted
   });
 });
