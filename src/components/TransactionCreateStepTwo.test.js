a3f2ebc4518fa7537455bee6e56846ffc652d4ba
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2021-11-19 23:17:22 -0300

    feat: joing tests to a logical approach

--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -2,14 +2,10 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import TransactionCreateStepTwo from "./TransactionCreateStepTwo";
 
-test("on initial render, the pay button is disabled", async () => {
+test("if an amount and note is entered, the pay button becomes enabled", async () => {
   render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "5" }} />);
 
   expect(await screen.findByRole("button", { name: /pay/i })).toBeDisabled();
-});
-
-test("if an amount and note is entered, the pay button becomes enabled", async () => {
-  render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "5" }} />);
 
   userEvent.type(screen.getByPlaceholderText(/amount/i), "50");
   userEvent.type(screen.getByPlaceholderText(/add a note/i), "dinner");
